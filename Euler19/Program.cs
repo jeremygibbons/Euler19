94242dad0c64bf9d0e8d184251ebfe1cb5b8c29a
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-09-01 06:08:56 +0200

    Initial valid solution

---
+++ Euler19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Euler19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint count = 0;
+            for(int y =  1901; y <= 2000; y++)
+            {
+                for(int m = 1; m <= 12; m++)
+                {
+                    DateTime d = new DateTime(y, m, 01);
+                    if(d.DayOfWeek == DayOfWeek.Sunday)
+                        count++;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.ReadLine();  
+        }
+    }
+}
